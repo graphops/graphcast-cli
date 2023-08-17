ffbc0e3d061c9502eb52f275a532b38a5b3cc501
hopeyen <hopey1118@gmail.com>
2023-08-17 13:24:01 -0500

    refactor: use latest sdk with query fix

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::sync::mpsc;
+
 use dotenv::dotenv;
+use graphcast_sdk::{graphcast_agent::GraphcastAgent, WakuMessage};
 use one_shot_cli::{config::Config, operator::RadioOperator, RADIO_OPERATOR};
 
 #[tokio::main]
@@ -7,8 +10,16 @@ async fn main() {
     // Parse basic configurations
     let radio_config = Config::args();
 
+    // The channel is not used in CLI
+    let (sender, _) = mpsc::channel::<WakuMessage>();
+    let agent = GraphcastAgent::new(
+        radio_config.to_graphcast_agent_config().await.unwrap(),
+        sender,
+    )
+    .await
+    .expect("Initialize Graphcast agent");
     // Initialization and pass in for static lifetime throughout the program
-    let radio_operator = RadioOperator::new(&radio_config).await;
+    let radio_operator = RadioOperator::new(&radio_config, agent).await;
 
     _ = RADIO_OPERATOR.set(radio_operator);
 
